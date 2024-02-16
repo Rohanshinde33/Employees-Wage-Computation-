c1babfa26abe8c4fb4e1a686108dbb4f217f165f
Rohamshinde33 <rohan337755@gmail.com>
2024-02-16 15:05:57 +0530

    [ADDED]:Ritesh- UC-1- completed for Employee Attedence

--- src/com/ritesh/employee/Employee_Project.java
+++ src/com/ritesh/employee/Employee_Project.java
@@ -1,10 +1,27 @@
 package com.ritesh.employee;
+
+import java.util.Random;
+
 public class Employee_Project {
 
 	public static void main(String[] args) {
+		int attendance=0;
 		
-	    System.out.println("Well come Employee Wage Computation Program on Master branch");
 		
+	    System.out.println("Well come Employee Wage Computation Program");
+
+		System.out.println("Enter the Attendence[0 - Absent /  1 - Present ]");
+		
+		Random random=new Random();
+		attendance=random.nextInt(9)%2;
+	    System.out.println(attendance);
+		
+		if(attendance==0) {
+			System.out.println("Employee Absent ");
+		}
+		else {
+			System.out.println("Employee present");
+			
 		}
 	}
 
