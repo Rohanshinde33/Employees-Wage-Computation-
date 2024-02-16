066507d28a528d46f4b711dde6094e934a6ac848
Rohamshinde33 <rohan337755@gmail.com>
2024-02-16 14:34:46 +0530

    [MODIFIED]:Ritesh- wellcome Massage show

--- src/com/ritesh/employee/Employee_Project.java
+++ src/com/ritesh/employee/Employee_Project.java
@@ -2,10 +2,8 @@ package com.ritesh.employee;
 public class Employee_Project {
 
 	public static void main(String[] args) {
-		int attendance=0;
 		
-		
-	    System.out.println("Enter the Attendence[0 - Absent /  1 - Present ]");
+	    System.out.println("Well come Employee Wage Computation Program on Master branch");
 		
 		}
 	}
