7933439ea1a323fc3b886e1b7d5e56fd26a5c10f
Rohamshinde33 <rohan337755@gmail.com>
2024-02-16 15:47:59 +0530

    [MODIFIED]:Ritesh- UC-1- completed for Employee Full Day Wage calculate

--- src/com/ritesh/employee/Employee_Project.java
+++ src/com/ritesh/employee/Employee_Project.java
@@ -5,7 +5,10 @@ import java.util.Random;
 public class Employee_Project {
 
 	public static void main(String[] args) {
-		int attendance=0;
+		int Attendance=0;
+		int Daily_wage=0;
+        final int Wage_Per_Hour=20;
+        final int Full_Day_Hour=8;
 		
 		
 	    System.out.println("Well come Employee Wage Computation Program");
@@ -13,14 +16,16 @@ public class Employee_Project {
 		System.out.println("Enter the Attendence[0 - Absent /  1 - Present ]");
 		
 		Random random=new Random();
-		attendance=random.nextInt(9)%2;
-	    System.out.println(attendance);
+		Attendance=random.nextInt(9)%2;
+	    System.out.println(Attendance);
 		
-		if(attendance==0) {
+		if(Attendance==0) {
 			System.out.println("Employee Absent ");
 		}
 		else {
 			System.out.println("Employee present");
+			Daily_wage=Wage_Per_Hour * Full_Day_Hour;
+			System.out.println("Full Day Wage = "+Daily_wage);
 			
 		}
 	}
