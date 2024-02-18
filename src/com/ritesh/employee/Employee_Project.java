a1854bedadb1a407f69191be32570d7516d1a723
Rohamshinde33 <rohan337755@gmail.com>
2024-02-18 21:11:59 +0530

    [MODIFIED]:Ritesh UC-5 Calculate Monthly Working Day

--- src/com/ritesh/employee/Employee_Project.java
+++ src/com/ritesh/employee/Employee_Project.java
@@ -11,44 +11,58 @@ public class Employee_Project {
         final int FULL_DAY_HOUR=8;
         final int PART_TIME_HOUR=4;
         int Employee_Type=0;
+        final int MONTLY_WAGE_COUNT=20;
         
         
 	    System.out.println("Well come Employee Wage Computation Program On Master Branch ");
-
-		System.out.println("Enter the Attendence[0 - Absent /  1 - Present ]");
 		
 		Random random=new Random();
 		Attendance=random.nextInt(9)%2;
+		
+		
+		for(int i = 1; i <= MONTLY_WAGE_COUNT; i++)
+		{
+			
+		//Random random=new Random();
+		//Attendance=random.nextInt(9)%2;
+			System.out.println("DAY "+i);
 	   
 		switch(Attendance) {
 		
 		case 0:
+			
 			System.out.println("Employee Absent ");
+			
 		   break;
 		
 		case 1:
+			
 			System.out.println("Employee present");
 			Employee_Type=random.nextInt(9)%2;
 
-			switch(Employee_Type) {
+			 switch(Employee_Type) {
 			
-			case 0:
+			 case 0:
 				
 				Daily_wage=WAGE_PER_HOUR*PART_TIME_HOUR;
 				System.out.println("Part Time Wage = "+Daily_wage);
 				
 				break;
 				
-			case 1:
+			 case 1:
 				
 				Daily_wage=WAGE_PER_HOUR * FULL_DAY_HOUR;
 				System.out.println("Full Day Wage = "+Daily_wage);
 				
 				break;
-			 }
+			  }
+			 
+			 break;
+			 
+		   }	
+		System.out.println("----------------");
 			
-			
-		 }
+		}
 	}
 
 }
